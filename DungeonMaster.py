502ffb55ebadf6c4d2036ef8c8aae51d6fbc5e73
OttoChu <ottoccl2004@gmail.com>
2024-04-14 04:17:57 +0100

    Added try to json file

--- DungeonMaster.py
+++ DungeonMaster.py
@@ -1,7 +1,6 @@
 import json
 import json_repair
 import re 
-import string
 import google.generativeai as genai
 
 class DungeonMaster():
@@ -22,51 +21,55 @@ class DungeonMaster():
         def clean_json_string(json_string):
             return json_string.replace('\x08', '').replace('\\b', '')
 
-        # Assuming self.situation is your JSON dictionary
-        with open('history.txt', 'a') as f:
-            json_string = json.dumps(self.situation)
-            cleaned_json_string = clean_json_string(json_string)
-            f.write(cleaned_json_string + '\n')
+        temp = clean_json_string(json.dumps(self.situation))
 
+        while True:
+            self.chat.send_message(
+                f"""
+                You are a Dungeon master for a text based Dungeons and Dragons game who responds entirely in JSON. Your response should contain the following fields:
 
-        self.chat.send_message(
-            f"""
-            You are a Dungeon master for a text based Dungeons and Dragons game who responds entirely in JSON. Your response should contain the following fields:
+                - current_health 
+                - current_stamina 
+                - current_situation 
+                - inventory
 
-            - current_health 
-            - current_stamina 
-            - current_situation 
-            - inventory
+                You also take the following inputs, in JSON format:
 
-            You also take the following inputs, in JSON format:
+                - current_health
+                - current_stamina
+                - current_situation
+                - action
+                - inventory
 
-            - current_health
-            - current_stamina
-            - current_situation
-            - action
-            - inventory
+                Do not respond with any information other than this raw JSON output or Markdown. Do not include any other information in your response. 
+                {self.situation}
+                """
+                )
+            if self.chat.history[-1].parts[0].text[0] == '`':
+                self.chat.history[-1].parts[0].text = '\n'.join(self.chat.history[-1].parts[0].text.split('\n')[1:-1])
+            # only change the double quotes to single quotes that are not a part of a word like apostrophes
+            self.chat.history[-1].parts[0].text = re.sub(r"(?<!\w)'(?!s\s)", '"', self.chat.history[-1].parts[0].text)
+            
+            self.chat.history[-1].parts[0].text = json_repair.repair_json(self.chat.history[-1].parts[0].text)
 
-            Do not respond with any information other than this raw JSON output or Markdown. Do not include any other information in your response. 
-            {self.situation}
-            """
-            )
-        if self.chat.history[-1].parts[0].text[0] == '`':
-           self.chat.history[-1].parts[0].text = '\n'.join(self.chat.history[-1].parts[0].text.split('\n')[1:-1])
-        # only change the double quotes to single quotes that are not a part of a word like apostrophes
-        self.chat.history[-1].parts[0].text = re.sub(r"(?<!\w)'(?!s\s)", '"', self.chat.history[-1].parts[0].text)
-        
-        self.chat.history[-1].parts[0].text = json_repair.repair_json(self.chat.history[-1].parts[0].text)
+            with open('response.json', 'w') as f:
+                f.write(self.chat.history[-1].parts[0].text)
 
-        with open('response.json', 'w') as f:
-            f.write(self.chat.history[-1].parts[0].text)
-        
-        with open('response.json', 'r') as f:
-            data = json.load(f)
+            with open('response.json', 'r') as f:
+                try:
+                    data = json.load(f)
+                    self.health = data["current_health"]
+                    self.stamina = data["current_stamina"]
+                    self.inventory = data["inventory"]
+                    self.situation = data
 
-        self.health = data["current_health"]
-        self.stamina = data["current_stamina"]
-        self.inventory = data["inventory"]
-        self.situation = data
+                    with open('history.txt', 'a') as f:
+                        f.write(temp + '\n')
+                    break
+
+                except (json.JSONDecodeError, KeyError):
+                    # Request another response from the language model
+                    continue
 
     def get_health(self):
         return self.situation['current_health']
